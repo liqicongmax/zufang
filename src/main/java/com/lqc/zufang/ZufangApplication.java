9de333606c1dbb1652c1920f66fe1d09547b29e0
a952267787MYAION <952267787@qq.com>
2019-02-25 09:34:04 +0800

    Springboot租房项目

---
+++ src/main/java/com/lqc/zufang/ZufangApplication.java
@@ -0,0 +1,13 @@
+package com.lqc.zufang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZufangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZufangApplication.class, args);
+    }
+
+}
