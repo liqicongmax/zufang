2334521e0457cd8d908e96906ecf4193e36c4ce5
a952267787MYAION <952267787@qq.com>
2019-02-26 14:14:43 +0800

    增加了新的注册登录页面以及注册登录功能

---
+++ src/main/java/com/lqc/zufang/constant/Identity.java
@@ -0,0 +1,10 @@
+package com.lqc.zufang.constant;
+
+/**
+ * @author: liqicong@myhexin.com
+ * @date 2019/2/26 11:18
+ */
+public class Identity {
+    public static final String ZUKE="租客";
+    public static final String FANGDONG="房东";
+}
